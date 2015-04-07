bc97e1950eb36aac991ec1fd4d6037c12f295fac
Petar Bogosavljevic <petar.bogosavljevic@siemens.com>
2015-04-07 09:51:22 +0200

    Initialcommit

---
+++ DevDB/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string body = "leer";
+
+            // start outlook
+            Outlook.Application outlookApplication = new Outlook.Application();
+
+
+            // get inbox
+            Outlook._NameSpace outlookNS = outlookApplication.Session;
+            Outlook.MAPIFolder inboxFolder = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["Posteingang"];
+            /*
+            Outlook.MAPIFolder einbuchung = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["[Gmail]"].Folders["DevDB"].Folders["Einbuchung"];
+            Outlook.MAPIFolder ausbuchung = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["[Gmail]"].Folders["DevDB"].Folders["Ausbuchung"];
+            */
+            Outlook.MAPIFolder einbuchung = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["Posteingang"].Folders["DevDB"].Folders["Einbuchung"];
+            Outlook.MAPIFolder ausbuchung = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["Posteingang"].Folders["DevDB"].Folders["Ausbuchung"];
+
+            foreach (COMObject item in inboxFolder.Items)
+            {
+                Outlook.MailItem mailItem = item as Outlook.MailItem;
+                if (mailItem != null && mailItem.Subject == "DevDB Einbuchung")
+                {
+                    body = mailItem.Body;
+                    mailItem.Move(einbuchung);
+                    mailItem.Save();
+                }
+                else if (mailItem != null && mailItem.Subject == "DevDB Ausbuchung")
+                {
+                    body = mailItem.Body;
+                    mailItem.Move(ausbuchung);
+                    mailItem.Save();
+                }
+            }
+
+            Console.WriteLine(body);
+
+            outlookApplication.Dispose();
+            Console.ReadKey();
+        }
+    }
+}
