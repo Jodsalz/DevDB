5a2265d466313f9eea0c973fa5801d7f4571fd98
Petar Bogosavljevic <petar.bogosavljevic@siemens.com>
2015-04-07 09:55:13 +0200

    Fixed bugs

--- DevDB/Program.cs
+++ DevDB/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NetOffice.OutlookApi.Enums;
+
+using NetOffice;
+using Outlook = NetOffice.OutlookApi;
 
 namespace DevDB
 {
