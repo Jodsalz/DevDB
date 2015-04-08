e2e1f3dd9bc5e3b747610fe2c60070bf5b12f3b8
Petar Bogosavljevic <petar.bogosavljevic@siemens.com>
2015-04-08 10:35:29 +0200

    Added Eventhandler

--- DevDB/Program.cs
+++ DevDB/Program.cs
@@ -12,45 +12,47 @@ namespace DevDB
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
             string body = "leer";
 
             // start outlook
-            Outlook.Application outlookApplication = new Outlook.Application();
+            NetOffice.OutlookApi.Application outlookApplication = new Outlook.Application();
+            
 
 
             // get inbox
             Outlook._NameSpace outlookNS = outlookApplication.Session;
-            Outlook.MAPIFolder inboxFolder = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["Posteingang"];
+            Outlook.MAPIFolder inboxFolder = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["Inbox"];
             /*
             Outlook.MAPIFolder einbuchung = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["[Gmail]"].Folders["DevDB"].Folders["Einbuchung"];
             Outlook.MAPIFolder ausbuchung = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["[Gmail]"].Folders["DevDB"].Folders["Ausbuchung"];
             */
-            Outlook.MAPIFolder einbuchung = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["Posteingang"].Folders["DevDB"].Folders["Einbuchung"];
-            Outlook.MAPIFolder ausbuchung = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["Posteingang"].Folders["DevDB"].Folders["Ausbuchung"];
+            Outlook.MAPIFolder einbuchung = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["[Gmail]"].Folders["DevDB"].Folders["Einbuchung"];
+            Outlook.MAPIFolder ausbuchung = outlookNS.Folders["devdb.mailhandler@gmail.com"].Folders["[Gmail]"].Folders["DevDB"].Folders["Ausbuchung"];
 
+            outlookApplication.NewMailExEvent += new Outlook.Application_NewMailExEventHandler(outlook_newmail);
+            
+        }
+
+        private static void outlook_newmail(string s)
+        {
             foreach (COMObject item in inboxFolder.Items)
             {
                 Outlook.MailItem mailItem = item as Outlook.MailItem;
                 if (mailItem != null && mailItem.Subject == "DevDB Einbuchung")
                 {
-                    body = mailItem.Body;
                     mailItem.Move(einbuchung);
-                    mailItem.Save();
+                    body = mailItem.Body;
                 }
                 else if (mailItem != null && mailItem.Subject == "DevDB Ausbuchung")
                 {
-                    body = mailItem.Body;
                     mailItem.Move(ausbuchung);
-                    mailItem.Save();
+                    body = mailItem.Body;
                 }
             }
-
-            Console.WriteLine(body);
-
-            outlookApplication.Dispose();
-            Console.ReadKey();
         }
     }
 }
